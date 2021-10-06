30f3b91bbcb4f4e74836279b36d6e05310cc1b2e
Jordan Cedeño <gatocedejordan2005@gamail.com>
2021-10-05 20:27:56 -0500

    Examen Supletorios 05/10/2021

---
+++ src/main/java1/examen/recuperacion/df/Cedenio/MainDiagramaFlujo.java
@@ -0,0 +1,18 @@
+package examen.recuperacion.df.Cedenio;
+import java.util.*;
+public class MainDiagramaFlujo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int d=1;
+		for(int i=1;i<=2;i++) {
+			for( int j=2;j>=i;j--) {
+			
+				System.out.print("*");
+				
+			}
+			System.out.println("");
+		}
+	}
+
+}
