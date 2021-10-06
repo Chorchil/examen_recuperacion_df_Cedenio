81bd8a41780e5ce98e3e6108e1375bf357e1e840
Jordan Cedeño <gatocedejordan2005@gamail.com>
2021-10-05 20:34:51 -0500

    Examen supletorios

--- src/main/java1/examen/recuperacion/df/Cedenio/MainDiagramaFlujo.java
+++ src/main/java1/examen/recuperacion/df/Cedenio/MainDiagramaFlujo.java
@@ -1,20 +1,26 @@
 package examen.recuperacion.df.Cedenio;
+
 import java.util.*;
+
 public class MainDiagramaFlujo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int menu=0;
-		do {
-		int d=1;
-		for(int i=1;i<=2;i++) {
-			for( int j=2;j>=i;j--) {
-			
-				System.out.print("*");
-				
+		int menu = 0;
+		Scanner lector=new Scanner(System.in);
+		do { System.out.println("Programa de figuras:\n1.Triangulo\n2.SALIR");
+		menu= lector.nextInt();
+		System.out.println("Ingrese dimension");
+			int d = lector.nextInt();
+			for (int i = 1; i <= d; i++) {
+				for (int j = d; j >= i; j--) {
+
+					System.out.print("*");
+
+				}
+				System.out.println("");
 			}
-			System.out.println("");
-		}
+		} while (menu != 2);
+
 	}
-	}while(menu)
 }
